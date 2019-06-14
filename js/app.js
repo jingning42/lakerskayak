e4c3f266e2fe6b3ced96e8b389154fd086226ab9
jenny42 <changchongning@gmail.com>
2019-06-14 23:30:41 +0800

    Site updated: 2019-06-14 23:30:40

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var appDaily = {
-  baiduSearch: function(words) {
+  bingSearch: function(words) {
     var key = 'site:' + window.location.host + '%20' + words.replace(/\s/g, '%20');
-    var url = 'https://www.baidu.com/baidu?tn=baidu&ie=utf-8&word=';
+    var url = 'https://cn.bing.com/search?q=';
 
     window.open(url + key, '_blank');
   },
@@ -14,8 +14,8 @@ var appDaily = {
   submitSearch: function(search_engines) {
     var $ipt = document.getElementById('homeSearchInput');
 
-    if (search_engines === 'baidu') {
-      this.baiduSearch($ipt.value.trim());
+    if (search_engines === 'bing') {
+      this.bingSearch($ipt.value.trim());
     } else {
       this.googleSearch($ipt.value.trim());
     }
